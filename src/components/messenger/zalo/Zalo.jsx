77ecaa9258666d412333ccb5e93c2aa432eb4aaa
chauhoaivu111 <chauhoaivu111@gmail.com>
2022-07-19 21:43:51 +0700

    dola

--- src/components/messenger/zalo/Zalo.jsx
+++ src/components/messenger/zalo/Zalo.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-
+import './Zalo.scss'
 import {Helmet} from 'react-helmet';
 
 const Zalo = () => {
   return (
-    <div>
+    <div className='zalo'>
 
         <Helmet>
         <script src="https://sp.zalo.me/plugins/sdk.js"></script>
         </Helmet>
-        <div class="zalo-chat-widget" data-oaid="3351945040214228269" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
-        
+     
+        <div class="zalo-chat-widget" data-oaid="3351945040214228269" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="1" data-width="300" data-height="300"></div>
+
+       
         
     </div>
   )
